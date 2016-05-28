66c009bc450e8fdc3abe5abd10ae9f79b3e8d3d1
befost <befost@umich.edu>
2016-05-27 20:38:36 -0700

    removed test

--- src/tile.rb
+++ src/tile.rb
@@ -11,14 +11,8 @@ class Tile
 		#@monsters = monster
 	end
 	
-	attr_accessor :passable, seen
+	attr_accessor :passable, :seen
 
-end
-
-class TestTiles < Test::Unit::TestCase
-	def test_simple
-		assert_equal(true, Tile.new(true).passable)
-	end
 end
 
 
