f986ec11ea732d8dcb2891b18aa0fa8b3ab73b22
befost <befost@umich.edu>
2016-05-31 23:48:20 -0700

    description

--- src/Map/tile.rb
+++ src/Map/tile.rb
@@ -1,12 +1,13 @@
 class Tile
 
-	def initialize(passable, events = [])
+	def initialize(passable, events = [], description = nil)
 		@passable = passable
 		@seen = false
 		@events = events
+		@description = description
 		#@monsters = monster
 	end
 
-	attr_accessor :passable, :seen, :events
+	attr_accessor :passable, :seen, :events, :description
 
 end
